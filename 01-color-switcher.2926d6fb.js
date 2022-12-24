5af33201d76b0137a8e23500d5402abe2ca74aa0
AndyDan1 <86267120+AndyDan1@users.noreply.github.com>
2022-12-24 12:14:00 +0000

    Deploying to gh-pages from @ AndyDan1/goit-js-hw-09@2b680a7d2f8a1704c7b83ce2c90e5e1bd6b13d92 🚀

---
+++ 01-color-switcher.2926d6fb.js
@@ -0,0 +1,2 @@
+!function(){var t,n=document.querySelectorAll("button");function o(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}n[0].addEventListener("click",(function(n){t=setInterval(o,500),o()})),n[1].addEventListener("click",(function(n){clearInterval(t)}))}();
+//# sourceMappingURL=01-color-switcher.2926d6fb.js.map
